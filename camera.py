c5e01daa3cfbff80a39cf89d649b4e16b60456a4
ben <ndaloben8@gmail.com>
2023-11-15 22:15:41 +0300

    file update

--- camera.py
+++ camera.py
@@ -16,13 +16,13 @@ class VideoCamera(object):
     def get_frame(self):
         # extracting frames
         ret, frame = self.video.read()
-        frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,
-                         interpolation=cv2.INTER_AREA)
-        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        face_rects=face_cascade.detectMultiScale(gray,1.3,5)
-        for (x,y,w,h) in face_rects:
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+        frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor,
+                           interpolation=cv2.INTER_AREA)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in face_rects:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             break
-        #encode OpenCV raw to jpg and displaying it 
-        ret,jpeg=cv2.imencode('.jpg',frame)
+        # encode OpenCV raw to jpg and displaying it
+        ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
