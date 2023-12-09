4110eafecf4a2209e83845b7c01f5d635cf24976
ben <ndaloben8@gmail.com>
2023-12-09 09:50:51 +0300

    updates

--- camera.py
+++ camera.py
@@ -1,28 +1,65 @@
 import cv2
+import time
+import datetime
 
-face_cascade = cv2.CascadeClassifier("faces.xml")
-ds_factor = 0.6
+# activate the camera
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
+detection = False
+detection_stopped_time = None
+timer_started = False
+SECONDS_TO_RECORD_AFTER_DETECTION = 5
+
+frame_size = (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # video format
 
 
-class VideoCamera(object):
-    def __init__(self):
-        # capturing video
-        self.video = cv2.VideoCapture(0)
+while True:
+    _, frame = cap.read()
 
-    def __del__(self):
-        # releasing camera
-        self.video.release()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+    bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    def get_frame(self):
-        # extracting frames
-        ret, frame = self.video.read()
-        frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor,
-                           interpolation=cv2.INTER_AREA)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x, y, w, h) in face_rects:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            break
-        # encode OpenCV raw to jpg and displaying it
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
+    if len(faces) + len(bodies) > 0:
+        if detection:
+            timer_started = False
+        else:
+            detection = True
+            current_time = datetime.datetime.now().strftime(
+                "%d-%m-%Y-%H-%M-%S")  # video file name
+            out = cv2.VideoWriter(
+                f"{current_time}.mp4", fourcc, 20, frame_size)
+            print("Started recording")
+    elif detection:
+        if timer_started:
+            if time.time() - detection_stopped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
+                detection = False
+                timer_started = False
+                out.release()
+                print('Stop Recording!')
+        else:
+            timer_started = True
+            detection_stopped_time = time.time()
+    if detection:
+        out.write(frame)
+
+    # blue rectangle covering face dimension
+    # for (x, y, width, height) in faces:
+    #     cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+
+    # display the camera frame
+    cv2.imshow("Camera", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# release the camera to be used by other programs
+out.release()
+cap.release()
+cv2.destroyAllWindows()  # close the windows
